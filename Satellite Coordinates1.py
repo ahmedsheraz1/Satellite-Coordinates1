39ec1bf6b17c8e5a49be1c1793f60a6a183c417b
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 18:03:31 +0500

    Delete Satellite Coordinates1.py

--- Satellite Coordinates1.py
+++
@@ -1,4 +0,0 @@
-Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
-'Coordinates: {latitude}, {longitude}'.format(latitude='30.3753N', longitude='-169.3451E')
-'Coordinates: 30.3753N, -169.3451E
