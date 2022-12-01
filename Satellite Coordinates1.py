b8de40e9f0caaf250d3724093050052a7186ecae
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 18:08:29 +0500

    Create Satellite Coordinates1.py

---
+++ Satellite Coordinates1.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+'Coordinates: {latitude}, {longitude}'.format(latitude='30.3753N', longitude='69.3451E')
+
+'Coordinates: 30.3753N, 69.3451E'
